1fc83e727b635b21a6545354a854e198b5557320
joostvanrijsinge <jjrijsin@student.avans.nl>
2014-04-16 11:35:35 +0200

    test edit on main

--- WordFeud/src/WordFeud/Main.java
+++ WordFeud/src/WordFeud/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Some Comments
-		System.out.println("Dit werkt voor geen ene meter, als dit aankomt neem ik mijn woorden terug");
+		System.out.println("Het werkt allemaal :D");
 	}
 
 }
