70c9e52fbd58a01deec6aef620f93ebac9cb1866
stijnvangaal <sjjgaal@student.avans.nl>
2014-04-16 11:54:06 +0200

    test stijn

--- WordFeud/src/WordFeud/Main.java
+++ WordFeud/src/WordFeud/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Some Comments
-		System.out.println("Het werkt allemaal :D");
+		System.out.println("Het werkt allemaal :D by stijn");
 	}
 
 }
