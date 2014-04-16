f748a7fe09d61bfcab417b8bb380527d6c023790
alecblom <agblom@student.avans.nl>
2014-04-16 12:53:19 +0200

    Initializatie

--- WordFeud/src/WordFeud/GameStone.java
+++ WordFeud/src/WordFeud/GameStone.java
@@ -2,4 +2,19 @@ package WordFeud;
 
 public class GameStone {
 
+	private int value;
+	private char letter;
+	
+	public GameStone(int value, char letter){
+		this.value = value;
+		this.letter = letter;
+	}
+	
+	public int getValue(){
+		return value;
+	}
+	
+	public char getLetter(){
+		return letter;
+	}
 }
--- WordFeud/src/WordFeud/Tile.java
+++ WordFeud/src/WordFeud/Tile.java
@@ -2,4 +2,47 @@ package WordFeud;
 
 public class Tile {
 
+	private String bonus;
+	private boolean bonusUsed;
+	private int xPos;
+	private int yPos;
+	private GameStone gameStone;
+	
+	public Tile(int x, int y){
+		this.xPos = x;
+		this.yPos = y;
+	}
+	
+	public String getBonus(){
+		return bonus;
+	}
+	
+	public GameStone getGameStone(){
+		return gameStone;
+	}
+	
+	public boolean getBonusUsed(){
+		return bonusUsed;
+	}
+	
+	public int getXPos(){
+		return xPos;
+	}
+	
+	public int getYPos(){
+		return yPos;
+	}
+	
+	public void setBonusUsed(boolean used){
+		this.bonusUsed = used;
+	}
+	
+	public void setCoordinates(int posX, int posY){
+		this.xPos = posX;
+		this.yPos = posY;
+	}
+	
+	public void setBonus(String bonus){
+		this.bonus = bonus;
+	}
 }
