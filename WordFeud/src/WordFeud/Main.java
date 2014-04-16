02fe2969ab1ac43141823bc7ce57d821147a59ba
stanvanheumen <cjgheume@student.avans.nl>
2014-04-16 10:53:50 +0200

    test terug

--- WordFeud/src/WordFeud/Main.java
+++ WordFeud/src/WordFeud/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Some Comments
-		System.out.println("Test");
+		System.out.println("Test terug");
 	}
 
 }
