4ae97e98d46104ff94396d68e1b9cd365e101ee7
stijnvangaal <sjjgaal@student.avans.nl>
2014-04-23 12:21:42 +0200

    comments added to game

--- WordFeud/src/WordFeud/Game.java
+++ WordFeud/src/WordFeud/Game.java
@@ -1,40 +1,89 @@
 package WordFeud;
+/*
+ * last update: 23/04
+ */
+import Utility.PointCounter;
+
+import Utility.WordChecker;
 
 public class Game {
 
+	Field myField;
+	private PointCounter myPC;
+	private WordChecker myWC;
+	
+	/**
+	 * cronstruct the game
+	 */
 	public Game(){
-		
+		myField = new Field();
 	}
 	
+	/**
+	 * tell field to lay a gamestone
+	 * let pointcounter calculate the points
+	 * @param gamestone
+	 * @param location
+	 * @return
+	 */
 	public boolean layGameStone(GameStone gamestone, String location){
 		return false;
 		
 	}
 	
+	/**
+	 * get the new words from the field
+	 * let the wordchecker check if it is correct
+	 */
 	public void playWord(){
 		
 	}
 	
+	/**
+	 * check if the game has ended
+	 * @return
+	 */
 	public boolean checkEndGame(){
 		return false;
 	}
 	
+	/**
+	 * pass a turn
+	 * tell the DB
+	 */
 	public void pass(){
 		
 	}
 	
+	/**
+	 * tell the DB to swap
+	 * get new gamestones
+	 */
 	public void swapGameStones(){
 		
 	}
 	
+	/**
+	 * shuffle your gamestones around
+	 * does not end turn
+	 */
 	public void shuffle(){
 		
 	}
 	
+	/**
+	 * set the visibility of a game
+	 * tell the db
+	 * @param bool
+	 */
 	public void setVisibility(Boolean bool){
 		
 	}
 	
+	/**
+	 * get the visibility from the db and return
+	 * @return
+	 */
 	public boolean getVisibility(){
 		return false;
 	}
