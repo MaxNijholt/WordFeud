be9f8ae258981668d670d3306c6979d15983929c
joostvanrijsinge <jjrijsin@student.avans.nl>
2014-04-23 12:30:33 +0200

    start on Player

--- WordFeud/src/AccountType/Player.java
+++ WordFeud/src/AccountType/Player.java
@@ -1,22 +1,30 @@
 package AccountType;
 
+import Utility.DBCommunicator;
 
 public class Player extends Account {
+	private String username;
 
-	
-	public Player(){
-		
+	public Player() {
+
 	}
-	
-	public void createCompetition(){
-		
+
+	public void createCompetition() {
+
 	}
-	
-	public void createGame(){
-		
+
+	public void createGame() {
+
 	}
-	
-	public void calculateStatistics(){
+
+	public void calculateStatistics() {
+
+		DBCommunicator
+				.requestData("SELECT spel_id, account_naam, sum(score) as totaal_score FROM mnijholt_db2.beurt as bleft join mnijholt_db2.spel as s on b.spel_id = s.idwhere s.toestand_type = 'Finished' and s.account_naam_tegenstander = "
+						+ username
+						+ " or s.toestand_type = 'Finished' and s.account_naam_uitdager = "
+						+ username + "group by account_naam");
 		
+
 	}
 }
