f06b60ab189a66307529a1ed6410562a783256fe
joostvanrijsinge <jjrijsin@student.avans.nl>
2014-04-23 10:01:29 +0200

    start on Moderator DBCommunicator coppied form stan

--- WordFeud/src/AccountType/Moderator.java
+++ WordFeud/src/AccountType/Moderator.java
@@ -1,13 +1,27 @@
 package AccountType;
 
+import Utility.DBCommunicator;
+
 
 public class Moderator extends Account {
+	
 
 	public Moderator(){
 		
 	}
 	
+	
+	/**
+	 * Gets unapproved words out of a list from the Database
+	 * 
+	 * @return
+	 */
 	public String[] getNotAprovedWords(){
+		
+		String query = "";
+		
+		DBCommunicator.requestData(query);
+		
 		return null;
 		
 	}
