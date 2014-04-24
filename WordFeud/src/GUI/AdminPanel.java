32eee0568c5fb00f3c18703b60e021ae97c5b2a8
Max Nijholt <m.nijholt@student.avans.nl>
2014-04-24 13:09:59 +0200

    First initialization for AdminPanel with JComboBox.

--- WordFeud/src/GUI/AdminPanel.java
+++ WordFeud/src/GUI/AdminPanel.java
@@ -1,7 +1,24 @@
 package GUI;
 
-public class AdminPanel extends Panel {
+import java.util.Vector;
 
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class AdminPanel extends Panel {
+	private JComboBox playerList;
+	private Vector<String> players;
+	private JLabel selectPlayer;
+	
+	
+	public AdminPanel(){
+		selectPlayer = new JLabel("Select player: ");
+		players = new Vector<String>();
+		this.playerList = new JComboBox(players);
+		this.add(playerList);
+	}
+	
 	
 	
 }
