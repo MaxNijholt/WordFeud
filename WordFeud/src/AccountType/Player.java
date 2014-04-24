86bcffdc21bd9c221041035411308b3d054a942f
joostvanrijsinge <jjrijsin@student.avans.nl>
2014-04-24 12:52:04 +0200

    AccountType player updated

--- WordFeud/src/AccountType/Player.java
+++ WordFeud/src/AccountType/Player.java
@@ -3,7 +3,10 @@ package AccountType;
 import Utility.DBCommunicator;
 
 public class Player extends Account {
-	private String username;
+	private String username = "jager684";
+
+	private double winLoss;
+	private int highScore;
 
 	public Player() {
 
@@ -19,11 +22,19 @@ public class Player extends Account {
 
 	public void calculateStatistics() {
 
-		DBCommunicator
-				.requestData("SELECT spel_id, account_naam, sum(score) as totaal_score FROM mnijholt_db2.beurt as bleft join mnijholt_db2.spel as s on b.spel_id = s.idwhere s.toestand_type = 'Finished' and s.account_naam_tegenstander = "
-						+ username
-						+ " or s.toestand_type = 'Finished' and s.account_naam_uitdager = "
-						+ username + "group by account_naam");
+		System.out
+				.println(DBCommunicator
+						.requestData("SELECT spel_id, account_naam, sum(score) as totaal_score FROM mnijholt_db2.beurt as b left join mnijholt_db2.spel as s on b.spel_id = s.id where s.toestand_type = 'Finished' and s.account_naam_tegenstander = "
+								+ "'"
+								+ username
+								+ "'"
+								+ " or s.toestand_type = 'Finished' and s.account_naam_uitdager = "
+								+ "'"
+								+ username
+								+ "'"
+								+ "group by account_naam"));
+		
+		
 		
 
 	}
