b3755f7b6b14674edabbac732240d4da5870c04f
Max Nijholt <m.nijholt@student.avans.nl>
2014-04-30 17:33:07 +0200

    Added competition to the game branch.

--- WordFeud/src/WordFeud/Competition.java
+++ WordFeud/src/WordFeud/Competition.java
@@ -4,7 +4,10 @@ import java.sql.Date;
 
 import AccountType.Player;
 
+
+@SuppressWarnings("unused")
 public class Competition {
+<<<<<<< HEAD
 
 	int myID;
 	public Competition(int id){
@@ -14,11 +17,54 @@ public class Competition {
 	public String getName(){
 		return "";
 		
+=======
+	private int id, min, max;
+	private String description, owner, startDate, endDate;
+
+	public Competition(int id){
+		this.id = id;
+		this.min = this.getMinimum();
+		this.max = this.getMaximum();
+		this.description = this.getCompetitionDescription();
+		this.owner = this.getCompetitionOwner();
+		this.startDate = this.getStartDate();
+		this.endDate = this.getEndDate();
 	}
 	
+	/*
+	 * Alternative method to add a competition. 
+	 */
+	public Competition(String compName, String endDate, String description, int mini, int maxi, String competitionOwner){
+		int lastID = DBCommunicator.requestInt("SELECT id FROM competitie ORDER BY id DESC");
+		int id = lastID + 1;
+		this.description = description;
+		this.min = mini;
+		this.max = maxi;
+		this.owner = competitionOwner;
+		DBCommunicator.writeData("INSERT INTO competitie (id, account_naam_eigenaar, start, einde, omschrijving, minimum_aantal_deelnemers, maximum_aantal_deelnemers) VALUES(" + this.id + ", '" + competitionOwner +"',  CURRENT_TIMESTAMP(), '" + endDate + "' , '" + description + "' , " + mini + "," + maxi + ");");
+	}
+	
+	/*
+	 * Getter to get the date when the compo ends.
+	 */
+	public String getEndDate(){
+		return DBCommunicator.requestData("SELECT einde FROM competitie WHERE id='" + this.id + "'");
+	}
+	/*
+	 * Getter to get the date the compo began.
+	 */
+	public String getStartDate(){
+		return DBCommunicator.requestData("SELECT einde FROM competitie WHERE id='" + this.id + "'");
+>>>>>>> 7825287... Last commit to the competition branch
+	}
+	
+	/*
+	 * Method to add a player to the competition
+	 */
 	public void addPlayer(Player player){
 		
 	}
+<<<<<<< HEAD
 	
 	public void setVisibility(Boolean bool){
 		
@@ -47,5 +93,34 @@ public class Competition {
 	
 	public int getID(){
 		return myID;
+=======
+
+	/*
+	 * Method to get the owner of the competition
+	 */
+	public String getCompetitionOwner() {
+		return DBCommunicator.requestData("SELECT account_naam_eigenaar FROM competitie WHERE id='" + this.id + "'");
+	}
+	
+	/*
+	 *	Getter for the description
+	 */
+	public String getCompetitionDescription() {
+		return DBCommunicator.requestData("SELECT omschrijving FROM competitie WHERE id='" + this.id + "'");
+	}
+	
+	/*
+	 * Getter for the minimum amount of players
+	 */
+	public int getMinimum(){
+		return DBCommunicator.requestInt("SELECT minimum_aantal FROM competitie WHERE id='" + this.id + "'");
+	}
+	
+	/*
+	 * Getter for the maximum amount of players
+	 */
+	private int getMaximum() {
+		return DBCommunicator.requestInt("SELECT maximum_aantal FROM competitie WHERE id='" + this.id + "'");
+>>>>>>> 7825287... Last commit to the competition branch
 	}
 }
