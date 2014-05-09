ca4080e9041ff14ccba2fced37e2d9ce7e9a374c
joostvanrijsinge <jjrijsin@student.avans.nl>
2014-05-09 09:13:17 +0200

    Moderator done Account done

--- WordFeud/src/AccountType/Account.java
+++ WordFeud/src/AccountType/Account.java
@@ -1,12 +1,13 @@
 package AccountType;
 
+import Utility.DBCommunicator;
+
 public abstract class Account {
-	private  String username;
+	private String username;
 
 	public Account(String username) {
 		this.username = username;
 	}
-
 
 	public String getUsername() {
 		return username;
@@ -14,14 +15,41 @@ public abstract class Account {
 	}
 
 	public boolean getIsModerator() {
+		if (DBCommunicator.requestData(
+				"SELECT rol_type FROM accountrol where account_naam = " + "'"
+						+ username + "'" + " and rol_type = 'Moderator'")
+				.equals("Moderator")) {
+
+			return true;
+		} else if (DBCommunicator.requestData("SELECT rol_type FROM accountrol where account_naam = " + "'"
+				+ username + "'" + " and rol_type = 'Moderator'") == null) {
+			return false;
+		}
 		return false;
 	}
 
 	public boolean getIsAdministrator() {
+		if (DBCommunicator.requestData("SELECT rol_type FROM accountrol where account_naam = " + "'"
+				+ username + "'" + " and rol_type = 'Aministrator'").equals("Administrator")) {
+
+			return true;
+		} else if (DBCommunicator.requestData("SELECT rol_type FROM accountrol where account_naam = " + "'"
+				+ username + "'" + " and rol_type = 'Aministrator'") == null) {
+			return false;
+		}
 		return false;
 	}
 
 	public boolean getIsPlayer() {
+
+		if (DBCommunicator.requestData("SELECT rol_type FROM accountrol where account_naam = " + "'"
+				+ username + "'" + " and rol_type = 'Player'").equals("Player")) {
+
+			return true;
+		} else if (DBCommunicator.requestData("SELECT rol_type FROM accountrol where account_naam = " + "'"
+				+ username + "'" + " and rol_type = 'Player'") == null) {
+			return false;
+		}
 		return false;
 	}
 
