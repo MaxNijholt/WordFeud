88d14d548c12d1ab9bab29649a8e0744ee36b544
jasperdelaat <j.delaat1@student.avans.nl>
2014-05-13 13:42:50 +0200

    Administrator aangepast

--- WordFeud/src/AccountType/Administrator.java
+++ WordFeud/src/AccountType/Administrator.java
@@ -1,5 +1,9 @@
 package AccountType;
 
+import java.util.ArrayList;
+
+import Utility.DBCommunicator;
+
 
 public class Administrator extends Account {
 
@@ -7,16 +11,27 @@ public class Administrator extends Account {
 		
 	}
 	
-	public void blacklistAccount(Account account){
+	public void addPermissions(String player, String right){
+		DBCommunicator.writeData("INSERT INTO accountrol (account_naam, rol_type) VALUES('"
+							+ player + "','" + right + "')");
 		
 	}
 	
-	public void addPermissions(Account account, String[] rolls){
-		
+	public void removePermissions(String player, String right){
+		DBCommunicator.writeData("DELETE FROM accountrol WHERE account_naam='"
+				+ player + "' AND rol_type='" + right + "'");
 	}
 	
-	public void removePermissions(Account account, String[] rolls){
+	public ArrayList<String> getUserRights(String player) {
+		ArrayList<String> data = DBCommunicator
+				.requestMoreData("SELECT rol_type FROM accountrol WHERE account_naam='"
+						+ player + "'");
+		return data;
+	}
+	
+	public void newPlayer(){
 		
 	}
+
 	
 }
