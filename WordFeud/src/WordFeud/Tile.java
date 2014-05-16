b9179e52c7d70dd75dc9a4c19a9febc38c2933e7
joostvanrijsinge <jjrijsin@student.avans.nl>
2014-05-16 10:08:31 +0200

    change constructors for field

--- WordFeud/src/WordFeud/Tile.java
+++ WordFeud/src/WordFeud/Tile.java
@@ -15,37 +15,71 @@ public class Tile extends SLabel {
 	private int xPos;
 	private int yPos;
 	private GameStone gameStone;
-	
-	public Tile(int x, int y){
+
+	public Tile(int x, int y) {
 		super("", SLabel.CENTER, 30, 30);
 		this.xPos = x;
 		this.yPos = y;
 		bonus = "";
 		bonusUsed = false;
 	}
-	
+
+	public Tile(int x, int y, String value) {
+		super("", SLabel.CENTER, 30, 30);
+		this.xPos = x;
+		this.yPos = y;
+		bonus = "";
+		bonusUsed = false;
+	}
+
 	public void paintComponent(Graphics g) {
-		Graphics2D g2d = (Graphics2D)g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-		
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
+				RenderingHints.VALUE_RENDER_QUALITY);
+
 		g2d.setColor(new Color(90, 90, 90));
 		g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 5, 5);
 	}
-	
+
 	// Getters
-	public String getBonus(){return bonus;}
-	public GameStone getGameStone(){return gameStone;}
-	public boolean getBonusUsed(){return bonusUsed;}
-	public int getXPos(){return xPos;}
-	public int getYPos(){return yPos;}
-	
+	public String getBonus() {
+		return bonus;
+	}
+
+	public GameStone getGameStone() {
+		return gameStone;
+	}
+
+	public boolean getBonusUsed() {
+		return bonusUsed;
+	}
+
+	public int getXPos() {
+		return xPos;
+	}
+
+	public int getYPos() {
+		return yPos;
+	}
+
 	// Setters
-	public void setBonusUsed(boolean used){this.bonusUsed = used;}
-	public void setGameStone(GameStone gamestone){
+	public void setBonusUsed(boolean used) {
+		this.bonusUsed = used;
+	}
+
+	public void setGameStone(GameStone gamestone) {
 		this.gameStone = gamestone;
 		repaint();
 	}
-	public void setCoordinates(int posX, int posY){this.xPos = posX; this.yPos = posY;}
-	public void setBonus(String bonus){this.bonus = bonus;}
+
+	public void setCoordinates(int posX, int posY) {
+		this.xPos = posX;
+		this.yPos = posY;
+	}
+
+	public void setBonus(String bonus) {
+		this.bonus = bonus;
+	}
 }
