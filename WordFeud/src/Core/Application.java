3ee05071975a9cca280828f9cd5b988814b95007
joostvanrijsinge <jjrijsin@student.avans.nl>
2014-05-16 10:05:36 +0200

    Field done I think

--- WordFeud/src/Core/Application.java
+++ WordFeud/src/Core/Application.java
@@ -395,20 +395,4 @@ public class Application {
 		this.setCurrentAccount(null);
 		myGui.switchPanel(new LoginPanel(myGui));
 	}
-<<<<<<< HEAD
-
-
-	public void selectGame(int gameID) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	public void spectateGame() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-=======
->>>>>>> 749691104f0e812f96ab62b12bd49804c25252e2
 }
