01271f392b019f72b229ec5266f0286e129cdc55
Alec <Alec@Alec-PC>
2014-05-19 15:11:49 +0200

    Added Tile constructor for GameStone

--- WordFeud/src/WordFeud/Tile.java
+++ WordFeud/src/WordFeud/Tile.java
@@ -32,6 +32,16 @@ public class Tile extends SLabel {
 		bonusUsed = false;
 	}
 
+	public Tile(int x, int y, GameStone stone){
+		super("", SLabel.CENTER, 32, 32);
+		this.xPos = x;
+		this.yPos = y;
+		bonus = "";
+		bonusUsed = false;
+		this.gameStone = stone;
+		
+	}
+	
 	public void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
