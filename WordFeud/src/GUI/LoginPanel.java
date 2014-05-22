94f70fc825ad39a3ad6e8595d65af9e9d3e5a47c
stanvanheumen <cjgheume@student.avans.nl>
2014-05-22 16:17:28 +0200

    added gameStone dragging! fixed LoginPanel/RegisterPanel/GameStone/Tile/SLabel for these changes

--- WordFeud/src/GUI/LoginPanel.java
+++ WordFeud/src/GUI/LoginPanel.java
@@ -18,6 +18,7 @@ import AccountType.Account;
 import Utility.DBCommunicator;
 import Utility.Loader;
 import Utility.SButton;
+import Utility.SLabel;
 import Utility.SPasswordField;
 import Utility.SPopupMenu;
 import Utility.STextField;
@@ -79,9 +80,9 @@ public class LoginPanel extends JPanel implements ActionListener {
 			if(DBCommunicator.checkConnection() != null) {
 				s = new GameStone(Integer.parseInt(Loader.TILEVALUES.get(String.valueOf(letters.charAt(i)))), letters.charAt(i));
 			}
-			s.setPreferredSize(new Dimension(80, 80));
 			s.setFonts(new Font("Arial", Font.BOLD, 55), new Font("Arial", Font.PLAIN, 20));
-			title.add(s);
+			s.setDimension(80, 80);
+			title.add(new SLabel(s.getImage(), 80, 80));
 		}
 		
 		JPanel mainPanel = new JPanel();
