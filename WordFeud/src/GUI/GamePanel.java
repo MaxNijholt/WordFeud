53b61d03b93c51a36bfcbe919412b6ad2941400f
tjvanos <tjos@student.avans.nl>
2014-05-27 15:20:55 +0200

    added score panel

--- WordFeud/src/GUI/GamePanel.java
+++ WordFeud/src/GUI/GamePanel.java
@@ -2,6 +2,7 @@ package GUI;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import Utility.DBCommunicator;
@@ -30,6 +32,7 @@ public class GamePanel extends JPanel implements Runnable, MouseListener,
 	private SButton pass, swap, resign, play, shuffle;
 	private ChatPanel cp;
 	private MenuPanel mp;
+	private JLabel score=new JLabel();
 	private Game game;
 	private GUI gui;
 	private GameStone currentGameStone;
@@ -37,9 +40,9 @@ public class GamePanel extends JPanel implements Runnable, MouseListener,
 	private Thread thread = new Thread(this);
 	private ArrayList<Tile> hand = new ArrayList<Tile>();
 	private ArrayList<Tile> field = new ArrayList<Tile>();
-	private int mouseX, mouseY;
+	private int mouseX, mouseY, turnScore=0;
 
-	public GamePanel(GUI gui){
+	public GamePanel(GUI gui) {
 		this.gui = gui;
 		this.game = gui.getApplication().getSelectedGame();
 		this.setPreferredSize(new Dimension(GUI.WIDTH, GUI.HEIGHT));
@@ -47,25 +50,25 @@ public class GamePanel extends JPanel implements Runnable, MouseListener,
 		this.setBackground(new Color(23, 26, 30));
 		this.addMouseListener(this);
 		this.addMouseMotionListener(this);
-		mp		= new MenuPanel(gui, new PlayerPanel(gui));
+		mp = new MenuPanel(gui, new PlayerPanel(gui));
 		mp.setPreferredSize(new Dimension(GUI.WIDTH, 30));
-		cp	 	= new ChatPanel(gui, game);
+		cp = new ChatPanel(gui, game);
 		cp.setPreferredSize(new Dimension(250, GUI.HEIGHT));
-		pass 	= new SButton("Pass", SButton.CYAN, 150, 40);
-		swap 	= new SButton("Swap", SButton.YELLOW, 150, 40);
-		resign 	= new SButton("Resign", SButton.RED, 150, 40);
-		play	= new SButton("Play", SButton.GREEN, 150, 40);
-		shuffle	= new SButton("Shuffle", SButton.PURPLE, 150, 40);
-		
+		pass = new SButton("Pass", SButton.CYAN, 150, 40);
+		swap = new SButton("Swap", SButton.YELLOW, 150, 40);
+		resign = new SButton("Resign", SButton.RED, 150, 40);
+		play = new SButton("Play", SButton.GREEN, 150, 40);
+		shuffle = new SButton("Shuffle", SButton.PURPLE, 150, 40);
+
 		pass.addActionListener(this);
 		swap.addActionListener(this);
 		resign.addActionListener(this);
 		play.addActionListener(this);
 		shuffle.addActionListener(this);
-		
-		//MenuPanel thread not stopping glitch fix:
+
+		// MenuPanel thread not stopping glitch fix:
 		mp.getBackButton().addActionListener(this);
-		
+
 		// The buttons
 		JPanel bp = new JPanel();
 		bp.setLayout(new GridLayout(5, 1, 0, 10));
@@ -75,40 +78,67 @@ public class GamePanel extends JPanel implements Runnable, MouseListener,
 		bp.add(resign);
 		bp.add(play);
 		bp.add(shuffle);
-		
+
+		// infopanel
+
+		score.setText("Your turn score will be: 0");
+		score.setOpaque(true);
+		score.setBackground(Color.GREEN);
+		score.setFont(new Font("Arial", Font.BOLD, 14));
+		JPanel infoPanel = new JPanel();
+		infoPanel.setLayout(new GridLayout(5, 1, 0, 10));
+		infoPanel.setPreferredSize(new Dimension(180, 215));
+		infoPanel.setBackground(new Color(33, 36, 40));
+		infoPanel.add(score);
+
 		add(mp);
-		mp.setBounds(0, 0, mp.getPreferredSize().width, mp.getPreferredSize().height);
+		mp.setBounds(0, 0, mp.getPreferredSize().width,
+				mp.getPreferredSize().height);
 		add(bp);
-		bp.setBounds(10, 50, bp.getPreferredSize().width, bp.getPreferredSize().height);
+		bp.setBounds(10, 50, bp.getPreferredSize().width,
+				bp.getPreferredSize().height);
+		add(infoPanel);
+		infoPanel.setBounds(10, 320, infoPanel.getPreferredSize().width, infoPanel.getPreferredSize().height);
 		add(cp);
-		cp.setBounds(GUI.WIDTH - cp.getPreferredSize().width - 10, 10, cp.getPreferredSize().width, cp.getPreferredSize().height);
-		
-		
+		cp.setBounds(GUI.WIDTH - cp.getPreferredSize().width - 10, 10,
+				cp.getPreferredSize().width, cp.getPreferredSize().height);
+
 		HashMap<String, Tile> tiles = game.getMyField().getTiles();
-		
-		for(int y = 1; y < 16; y++) {
-			for(int x = 1; x < 16; x++) {
+
+		for (int y = 1; y < 16; y++)
+		{
+			for (int x = 1; x < 16; x++)
+			{
 				Tile tile = tiles.get(x + "," + y);
 				field.add(tile);
 				tile.setPickablity(false);
 			}
 		}
-				
-		ArrayList<GameStone> currentGameStones = DBCommunicator.getHandLetters(game.getID(), DBCommunicator.requestInt("SELECT id from beurt WHERE spel_id = " + game.getID() + " AND account_naam = '" + gui.getApplication().getCurrentAccount().getUsername() + "' ORDER BY id DESC"), Loader.getGameStones("EN"));
-		
-		for(int i = 0; i < 7; i++) {
+
+		ArrayList<GameStone> currentGameStones = DBCommunicator.getHandLetters(
+				game.getID(), DBCommunicator
+						.requestInt("SELECT id from beurt WHERE spel_id = "
+								+ game.getID()
+								+ " AND account_naam = '"
+								+ gui.getApplication().getCurrentAccount()
+										.getUsername() + "' ORDER BY id DESC"),
+				Loader.getGameStones("EN"));
+
+		for (int i = 0; i < 7; i++)
+		{
 			Tile tile = new Tile(i + 1, -1);
-			if(i < currentGameStones.size()) {
-				if(currentGameStones.get(i) != null) {
+			if (i < currentGameStones.size())
+			{
+				if (currentGameStones.get(i) != null)
+				{
 					tile.setGameStone(currentGameStones.get(i));
 					tile.getGameStone().setHand(true);
-					
+
 				}
 			}
 			hand.add(tile);
 		}
 
-		
 		thread.start();
 	}
 
@@ -184,8 +214,10 @@ public class GamePanel extends JPanel implements Runnable, MouseListener,
 						{
 							currentGameStone = t.getGameStone();
 							t.setPickablity(false);
-							System.out.println(gui.removeGameStone(t.getXPos()
-									+ "," + t.getYPos()));
+							score.setText("Your turn score would be: "
+									+ gui.layGameStone(currentGameStone,
+											(t.getXPos() + "," + t.getYPos())));
+
 							t.setGameStone(null);
 						}
 					}
@@ -194,9 +226,9 @@ public class GamePanel extends JPanel implements Runnable, MouseListener,
 						if (t.getGameStone() == null)
 						{
 							t.setGameStone(currentGameStone);
-							System.out.println(gui.layGameStone(
-									currentGameStone,
-									(t.getXPos() + "," + t.getYPos())));
+							score.setText("Your turn score would be: "
+									+ gui.layGameStone(currentGameStone,
+											(t.getXPos() + "," + t.getYPos())));
 							t.setPickablity(true);
 							currentGameStone = null;
 						}
@@ -242,19 +274,23 @@ public class GamePanel extends JPanel implements Runnable, MouseListener,
 		{
 			running = false;
 		}
-		if (e.getSource().equals(shuffle)) {
+		if (e.getSource().equals(shuffle))
+		{
 			game.shuffle();
 
-			for (int i = 0; i < field.size(); i++) {
-				if (field.get(i).getGameStone() != null) {
-					if (field.get(i).getGameStone().getHand()) {
+			for (int i = 0; i < field.size(); i++)
+			{
+				if (field.get(i).getGameStone() != null)
+				{
+					if (field.get(i).getGameStone().getHand())
+					{
 						field.get(i).setGameStone(null);
 					}
 				}
 			}
-			
+
 			Collections.shuffle(hand);
-			
+
 			currentGameStone = null;
 		}
 		if (e.getSource().equals(swap))
@@ -286,4 +322,13 @@ public class GamePanel extends JPanel implements Runnable, MouseListener,
 		mouseY = e.getY();
 	}
 
+	public int getTurnScore()
+	{
+		return turnScore;
+	}
+
+	public void setTurnScore(int turnScore)
+	{
+		this.turnScore = turnScore;
+	}
 }
