23871b4ebdaec0543ca6b86a634d913cd4386be4
Max Nijholt <m.nijholt@student.avans.nl>
2014-05-28 09:02:58 +0200

    Finished Moderator Test. Player is finished. (Empty). Also fixed Moderator to works with the tests.

--- WordFeud/src/AccountType/Player.java
+++ WordFeud/src/AccountType/Player.java
@@ -1,26 +1,9 @@
 package AccountType;
 
-import Utility.DBCommunicator;
-
-public class Player{
+public class Player {
 	Account account;
-	
+
 	public Player(Account account) {
 		this.account = account;
-	}
-
-	public void calculateStatistics() {
-		System.out
-				.println(DBCommunicator
-						.requestData("SELECT spel_id, account_naam, sum(score) as totaal_score FROM mnijholt_db2.beurt as b left join mnijholt_db2.spel as s on b.spel_id = s.id where s.toestand_type = 'Finished' and s.account_naam_tegenstander = "
-								+ "'"
-								+ account.getUsername()
-								+ "'"
-								+ " or s.toestand_type = 'Finished' and s.account_naam_uitdager = "
-								+ "'"
-								+ account.getUsername()
-								+ "'"
-								+ "group by account_naam"));
-
 	}
 }
