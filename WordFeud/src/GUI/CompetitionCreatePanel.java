b2b38c072673f13b638efbf42c9136d07e2eec16
alecblom <agblom@student.avans.nl>
2014-06-02 11:33:20 +0200

    full comps dont show up anymore

--- WordFeud/src/GUI/CompetitionCreatePanel.java
+++ WordFeud/src/GUI/CompetitionCreatePanel.java
@@ -13,13 +13,10 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
-import Utility.AScrollPane;
 import Utility.DBCommunicator;
-import Utility.MComboBox;
 import Utility.SButton;
 import Utility.SComboBox;
 import Utility.SLabel;
@@ -28,17 +25,15 @@ import Utility.STextField;
 @SuppressWarnings("serial")
 public class CompetitionCreatePanel extends JPanel implements ActionListener {
 
-	private STextField 			name, player, endDate, year, day, month;;
+	private STextField 			name, player, year, day, month;;
 	private SComboBox 			addPlayers;
-	private SLabel 				nameLabel, playerLabel, endDateLabel, addedLabel;
-	private AScrollPane			scroller;
-	private ArrayList<SLabel>	addedPlayers;
-	private SButton 			create, back, add;
-	private JPanel				addPanel, addedPanel;
+	private SLabel 				nameLabel, playerLabel, endDateLabel;
+	private SButton 			create;
 	private GUI 				gui;
 	private JPanel scrollPane;
 	private MenuPanel mp;
-	private Calendar calendar;
+	private JPanel buttonPanel;
+	private GridBagConstraints c;
 	
 	public CompetitionCreatePanel(GUI gui) {
 		this.gui = gui;
@@ -48,26 +43,21 @@ public class CompetitionCreatePanel extends JPanel implements ActionListener {
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		this.setBackground(new Color(94, 94, 94));
 		
-		addedPlayers	= new ArrayList<SLabel>();
 		
-//		title 			= new SLabel("New competition", SLabel.LEFT, new Font("Arial", Font.BOLD, 50));
 		nameLabel		= new SLabel("Competition name", SLabel.LEFT, new Font("Arial", Font.PLAIN, 15), 220, 20);
 		playerLabel		= new SLabel("Maximum players", SLabel.LEFT, new Font("Arial", Font.PLAIN, 15), 220, 20);
 		endDateLabel	= new SLabel("End date", SLabel.LEFT, new Font("Arial", Font.PLAIN, 15), 220, 20);
-		addedLabel		= new SLabel("Added players", SLabel.LEFT, 220, 40);
 		
 		create 			= new SButton("Create", SButton.GREY, 220, 40);
-		back 			= new SButton("Back", SButton.GREY, 220, 40);
-		add 			= new SButton("Add players", SButton.GREY, 220, 40);
 		
 		name 			= new STextField("Competition name", 220, 40);
-		player 			= new STextField("Maximum players (2 up to 24)", 220, 40);
-		endDate 		= new STextField("YYYY-MM-DD", 220, 40);
+		player 			= new STextField("Maximum players (up to 24)", 220, 40);
 		year  			= new STextField("YYYY", 70, 40);
 		month  			= new STextField("MM", 70, 40);
 		day  			= new STextField("DD", 70, 40);
 		
 		mp				= new MenuPanel(gui, "CompetitionPanel");
+		buttonPanel		= new JPanel();
 		
 		// To fill the challenger box
 				ArrayList<String> allPlayers = DBCommunicator.requestMoreData("SELECT naam FROM account ORDER BY naam ASC");
@@ -80,8 +70,7 @@ public class CompetitionCreatePanel extends JPanel implements ActionListener {
 		scrollPane = new JPanel();
 		scrollPane.setLayout(new GridLayout(0, 1));
 		scrollPane.setBackground(getBackground());
-		
-		scroller 		= new AScrollPane(100, 100, scrollPane, false, true);
+
 		for(int i = 0; i < 20; i++) {
 			SButton l = new SButton("Sla" + i, SButton.WHITE);
 			l.setCustomRounded(false, false, false, false);
@@ -91,14 +80,12 @@ public class CompetitionCreatePanel extends JPanel implements ActionListener {
 		}
 		
 		
-		back.addActionListener(this);
-		add.addActionListener(this);
 		create.addActionListener(this);
 		
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new GridBagLayout());
 		buttonPanel.setBackground(getBackground());
-		GridBagConstraints c = new GridBagConstraints();
+		c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 0;
 		c.insets = new Insets(0, 15, 5, 0);
@@ -121,39 +108,16 @@ public class CompetitionCreatePanel extends JPanel implements ActionListener {
 		buttonPanel.add(day, c);
 		c.gridx = 0;
 		c.gridy++;
-//		buttonPanel.add(playerLabel, c);
 		c.gridy++;
-//		buttonPanel.add(player, c);
 		c.gridy++;
 		buttonPanel.add(create, c);
-//		buttonPanel.add(back, c);
 		
-//		JPanel titlePanel = new JPanel();
-//		titlePanel.setBackground(new Color(0, 0, 0, 100));
-//		titlePanel.setLayout(new BorderLayout());
-//		titlePanel.add(title, BorderLayout.WEST);
-//		titlePanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-		
-		addPanel = new JPanel();
-		addPanel.setBackground(getBackground());
-		addPanel.setLayout(new BoxLayout(addPanel, BoxLayout.PAGE_AXIS));
-		for(int i = 0; i < addedPlayers.size(); i++) {
-			addPanel.add(addedPlayers.get(i));
-		}
-		
-//		addedPanel = new JPanel();
-//		addedPanel.setBackground(getBackground());
-//		addedPanel.setLayout(new BorderLayout());
-//		addedPanel.add(addedLabel, BorderLayout.NORTH);
-//		addedPanel.add(scroller, BorderLayout.CENTER);
-//		addedPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
 		
 		JPanel mainPanel = new JPanel();
 		mainPanel.setBackground(getBackground());
 		mainPanel.setLayout(new BorderLayout());
 		mainPanel.add(mp, BorderLayout.NORTH);
 		mainPanel.add(buttonPanel, BorderLayout.WEST);
-//		mainPanel.add(addedPanel, BorderLayout.EAST);
 		
 		this.add(mainPanel);
 		gui.setLoadingCursor(false);
@@ -210,11 +174,15 @@ public class CompetitionCreatePanel extends JPanel implements ActionListener {
 		return false;
 	}
 	
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource().equals(back)) {
-			gui.switchPanel(new CompetitionPanel(gui));
-		}
-		
+	public void showError(){
+		SLabel error = new SLabel("Please check if you filled in all fields correctly", SLabel.LEFT);
+		error.setBackground(Color.red);
+		c.gridx++;
+		buttonPanel.add(error, c);
+		revalidate();
+	}
+	
+	public void actionPerformed(ActionEvent e) {	
 		if(e.getSource().equals(create)){
 			
 			if(!name.getText().isEmpty() && !player.getText().isEmpty() && getInt(player.getText()) > 1 && !year.getText().isEmpty() 
@@ -228,42 +196,15 @@ public class CompetitionCreatePanel extends JPanel implements ActionListener {
 				gui.switchPanel(new CompetitionPanel(gui));
 				}			
 				else{
-					System.out.println("Please check if you filled in all fields correctly");
+					System.out.println("nope");
+					showError();
 				}
 			}
 			else{
-				System.out.println("Please check if you filled in all fields correctly");
+				System.out.println("nope");
+				showError();
 			}	
 			
-		}
-		
-		if(e.getSource().equals(add)) {
-			boolean alreadyThere = false;
-			for(int x = 0; x < scrollPane.getComponents().length; x++) {
-				SButton s = (SButton)scrollPane.getComponent(x);
-				if(e.getActionCommand().equals(s.getActionCommand())) {
-					alreadyThere = true;
-					return;
-				}
-			}
-			if(!alreadyThere) {
-				SButton l = new SButton(addPlayers.getSelectedItem(), SButton.WHITE);
-				l.setCustomRounded(false, false, false, false);
-				l.setTextColor(Color.BLACK);
-				l.addActionListener(this);
-				scrollPane.add(l);
-				scrollPane.revalidate();
-			}
-		}
-		for(int i = 0; i < scrollPane.getComponents().length; i ++) {
-			if(scrollPane.getComponent(i) instanceof SButton) {
-				SButton s = (SButton)scrollPane.getComponent(i);
-				if(s.getActionCommand().equals(e.getActionCommand())) {
-					scrollPane.remove(scrollPane.getComponent(i));
-					scrollPane.revalidate();
-					return;
-				}
-			}
 		}
 	}
 	
