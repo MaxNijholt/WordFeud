ff2bb966b0f0adbdcb51aafd16e79bcce33dda71
Max Nijholt <m.nijholt@student.avans.nl>
2014-06-02 14:14:22 +0200

    Window close on lose focus

--- WordFeud/src/GUI/SpectatorPanel.java
+++ WordFeud/src/GUI/SpectatorPanel.java
@@ -11,6 +11,8 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
 import javax.swing.Box;
@@ -138,6 +140,15 @@ public class SpectatorPanel extends JPanel {
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
 					final JFrame playerFrame = new JFrame();
+					playerFrame.addWindowListener(new WindowAdapter() {
+			            //
+			            // Invoked when a window is de-activated.
+			            //
+			            public void windowDeactivated(WindowEvent e) {
+			            	playerFrame.dispose();
+			            }
+			 
+			        });
 					JPanel playerPanel = new JPanel();
 					
 					playerPanel.setLayout(new BoxLayout(playerPanel, BoxLayout.PAGE_AXIS));
