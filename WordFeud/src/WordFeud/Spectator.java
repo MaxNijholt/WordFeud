0fa5ac61b1e420972788dae70a5955ba246b6b6c
joostvanrijsinge <jjrijsin@student.avans.nl>
2014-06-03 12:15:22 +0200

    SpectatorInfoPanel FTW!!!!!!!!

--- WordFeud/src/WordFeud/Spectator.java
+++ WordFeud/src/WordFeud/Spectator.java
@@ -3,55 +3,55 @@ package WordFeud;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import GUI.SpectatorInfoPanel;
 import Utility.DBCommunicator;
 import Utility.Loader;
 
 public class Spectator {
 	private Field myField;
 	private int myGameID, lastTurn, turn;
-	private String letters;
-	private ArrayList<Integer> gameStones;
-	private HashMap<Integer, Character> stoneChars;
+	private String letters, turnText;
 	private ArrayList<GameStone> handStones;
-	
-	public Spectator(int gameID){
+
+	public Spectator(int gameID) {
 		myGameID = gameID;
 		myField = new Field(myGameID);
-		gameStones = new ArrayList<Integer>();
-		stoneChars = new HashMap<Integer, Character>();
-		
-		
+
 	}
-	
-	public int getLastTurn(){
-		lastTurn = DBCommunicator.requestInt("SELECT id from beurt WHERE spel_id = " + myGameID + " AND aktie_type != 'End' ORDER BY id DESC");
+
+	public int getLastTurn() {
+		lastTurn = DBCommunicator
+				.requestInt("SELECT id from beurt WHERE spel_id = " + myGameID
+						+ " AND aktie_type != 'End' ORDER BY id DESC");
 		return lastTurn;
 	}
-	
-	public String getTurnLetters(int turn){
-		if(turn == DBCommunicator.requestInt("SELECT id from beurt WHERE spel_id = " + myGameID + " AND id = " + turn + " ORDER BY id DESC")){
-			letters = DBCommunicator.requestData("SELECT inhoud FROM plankje WHERE spel_id = " + myGameID + " AND beurt_id = " + turn);
+
+	public String getTurnLetters(int turn) {
+		if (turn == DBCommunicator
+				.requestInt("SELECT id from beurt WHERE spel_id = " + myGameID
+						+ " AND id = " + turn + " ORDER BY id DESC")) {
+			letters = DBCommunicator
+					.requestData("SELECT inhoud FROM plankje WHERE spel_id = "
+							+ myGameID + " AND beurt_id = " + turn);
 			return letters;
-		}
-		else{
+		} else {
 			System.out.println("invalid turn");
 			return null;
 		}
-		
+
 	}
-	
 
 	public Field getMyField() {
 		myField.updateField(myGameID);
 		return myField;
 	}
-	
-	
-	public int getCompID(int gameID){
-		int compID = DBCommunicator.requestInt("SELECT competitie_id FROM spel where id = '"+ gameID + "'");
+
+	public int getCompID(int gameID) {
+		int compID = DBCommunicator
+				.requestInt("SELECT competitie_id FROM spel where id = '"
+						+ gameID + "'");
 		return compID;
-	}	
-	
+	}
 
 	public int getTurn() {
 		return turn;
@@ -60,12 +60,32 @@ public class Spectator {
 	public void setTurn(int turn) {
 		this.turn = turn;
 	}
-	
-	public ArrayList<GameStone> getHand(int turn){
+
+	public ArrayList<GameStone> getHand(int turn) {
 		handStones = Loader.getGameStones("EN");
 		handStones = DBCommunicator.getHandLetters(myGameID, turn, handStones);
 		return handStones;
 	}
+
+	public String turnText(int gameID, int turn){
+
+		String playerName = DBCommunicator.requestData("SELECT account_naam FROM beurt where spel_id = '" + gameID + "' and id = '" + turn + "'");
+		
+		if(DBCommunicator.requestData("SELECT aktie_type FROM beurt where spel_id = '" + gameID + "' and id = '" + turn + "'").equals("Swap")){
+			
+			turnText = playerName + " Swapped";
+		}	else if(turn == 2){
+			turnText = "First turn";
+		}	else{
+			String playedWord = DBCommunicator.requestData("SELECT woorddeel FROM gelegd where spel_id = '" + gameID + "' and beurt_id = '" + turn + "'");
+			int points = DBCommunicator.requestInt("SELECT score FROM beurt where spel_id = '" + gameID + "' and id = '" + turn + "'");
 	
-	
+			playedWord = playedWord.replaceAll(",", "");
+			
+			
+			turnText = playerName + " played: " + playedWord + " for: " + points + " points";
+		}
+		System.out.println(turnText);
+		return turnText;
+	}
 }
